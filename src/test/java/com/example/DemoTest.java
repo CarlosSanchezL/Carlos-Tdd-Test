4c7e64ab3687e896163d37e364bacc33aedd6800
Carlos Sanchez <A01741201@tec.mx>
2025-04-07 08:59:50 -0600

    Corregido

---
+++ src/test/java/com/example/DemoTest.java
@@ -0,0 +1,13 @@
+
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoTest {
+
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
